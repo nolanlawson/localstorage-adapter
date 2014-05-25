2d428a494ecd1d5863e45d06efd57aa1a2c80ccf
Nolan Lawson <nolan.lawson@gmail.com>
2014-05-25 12:18:39 -0700

    WIP

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var adapter = require('pouchdb-abstract-adapter');
-
 var leveldown = require('localstorage-down');
 leveldown.adapterName = 'localstorage';
 leveldown.valid = function () {
@@ -9,4 +7,4 @@ leveldown.valid = function () {
 }
 leveldown.usePrefix = true;
 
-module.exports = adapter(leveldown);
+module.exports = leveldown;
